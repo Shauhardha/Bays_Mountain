4a075810fa490f53829b54128bbd0617d848235e
Shauhardha <sauharda.kshatri@gmail.com>
2025-04-07 01:00:16 -0400

    mistake

--- database/postgresql_connection.py
+++ database/postgresql_connection.py
@@ -6,7 +6,7 @@ def init_postgres_connection():
     Initializes a connection to the PostgreSQL database.
     """
     try:
-        conn = psycopg2.connect(
+        conn = psycopg2.connect_(
             host="localhost",  # Replace with your PostgreSQL host
             database="Bays_Mountain",  # Replace with your PostgreSQL database name
             user="bays_owner",  # Replace with your PostgreSQL username
@@ -18,7 +18,7 @@ def init_postgres_connection():
         raise
 
 
-def init_postgres_connection_():
+def init_postgres_connection():
     """
     Initializes a connection to the PostgreSQL database.
     """
